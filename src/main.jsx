80042afeb66f54c88003b9f493f85c9f59780ff0
Vivek Sawant <vicky210490@gmail.com>
2024-06-20 15:18:53 +0530

    proivde store to application

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
+// imports from third party libraries
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+// imports from project
 import "./index.css";
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
