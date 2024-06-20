827ff99d04cfca7a7d7de7bdc1f200edc14e4c10
Vivek Sawant <vicky210490@gmail.com>
2024-06-20 15:18:08 +0530

    define supplier route

--- src/routes/suppliers/suppliers.component.jsx
+++ src/routes/suppliers/suppliers.component.jsx
@@ -1,7 +1,115 @@
+// imports from third party libraries
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+// imports from project
+import {
+  SupplierListContainer,
+  FormContainer,
+  SupplierContainer,
+  AddSupplierForm,
+} from "../../routes/suppliers/suppliers.styles";
+import FormInput from "../../components/form-input/form-input.component";
+import Button from "../../components/button/button.component";
+import { saveSupplier } from "../../store/supplier/supplier.slice";
+
 const Suppliers = () => {
+  const [supplierName, setSupplierName] = useState("");
+  const [whatsappNumber, setWhatsappNumber] = useState("");
+  const [alternateNumber, setAlternateNumber] = useState("");
+  const dispatch = useDispatch();
+
+  // reset form fields
+  const resetFormFields = () => {
+    setSupplierName("");
+    setWhatsappNumber("");
+    setAlternateNumber("");
+  };
+
+  // handle change
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "supplierName") {
+      setSupplierName(value);
+    } else if (name === "whatsappNumber") {
+      setWhatsappNumber(value);
+    } else if (name === "alternateNumber") {
+      setAlternateNumber(value);
+    }
+  };
+
+  // handle submit
+  const handlesubmit = (event) => {
+    event.preventDefault();
+
+    // validate form
+    if (!supplierName || !whatsappNumber) {
+      alert("Please enter supplier name and whatsapp number");
+      return;
+    } else if (whatsappNumber.length !== 10) {
+      alert("Please enter valid whatsapp number");
+      return;
+    } else if (whatsappNumber === alternateNumber) {
+      alert("Please enter different alternate whatsapp number");
+      return;
+    }
+
+    // // create phone number list
+    // const phoneNumberList = [
+    //   whatsappNumber,
+    //   alternateNumber && alternateNumber.trim() ? alternateNumber : "", // Only add non-empty alternate number
+    // ];
+
+    // create supplier object
+    const supplier = {
+      name: supplierName,
+      whatsappNumber: whatsappNumber,
+      alternateNumber: alternateNumber ? alternateNumber : null,
+    };
+
+    console.log(supplier);
+
+    // dispatch save supplier
+    dispatch(saveSupplier(supplier));
+
+    // reset form
+    resetFormFields();
+  };
+
   return (
     <div>
-      <h2>Suppliers</h2>
+      <SupplierContainer>
+        <SupplierListContainer></SupplierListContainer>
+        <FormContainer>
+          <h3>Add Supplier</h3>
+          <AddSupplierForm onSubmit={handlesubmit}>
+            <FormInput
+              label="Supplier Name"
+              type="text"
+              required
+              onChange={handleChange}
+              name="supplierName"
+              value={supplierName}
+            />
+            <FormInput
+              label="Whatsapp Number"
+              type="text"
+              required
+              onChange={handleChange}
+              name="whatsappNumber"
+              value={whatsappNumber}
+            />
+            <FormInput
+              label="Alternate Whatsapp Number"
+              type="text"
+              onChange={handleChange}
+              name="alternateNumber"
+              value={alternateNumber}
+            />
+            <Button type="submit">Add</Button>
+          </AddSupplierForm>
+        </FormContainer>
+      </SupplierContainer>
     </div>
   );
 };
---
+++ src/routes/suppliers/suppliers.styles.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const SupplierContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 2rem;
+`;
+
+export const SupplierListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 50%;
+  padding: 0 5rem;
+`;
+
+export const SupplierName = styled.h3`
+  font-size: 2rem;
+  font-weight: bold;
+  /* margin-bottom: 1rem; */
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50%;
+  padding: 0 5rem;
+`;
+
+export const AddSupplierForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
