f87e1f08804057889f0c5fe0f0b24273cbe0d04d
Vivek Sawant <vicky210490@gmail.com>
2024-06-21 15:38:45 +0530

    add custom alert

--- src/store/supplier/supplier.slice.js
+++ src/store/supplier/supplier.slice.js
@@ -1,6 +1,7 @@
 // imports from third party libraries
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 // default state
 const initialState = {
@@ -13,7 +14,6 @@ const initialState = {
 export const saveSupplier = createAsyncThunk(
   "supplier/saveSupplier",
   async (supplier) => {
-    console.log(supplier);
     return await axios
       .post("http://localhost:9000/supplier", supplier)
       .then((response) => response.data);
@@ -23,6 +23,11 @@ export const saveSupplier = createAsyncThunk(
 const supplierSlice = createSlice({
   name: "supplier",
   initialState,
+  reducers: {
+    resetError: (state) => {
+      state.error = "";
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(saveSupplier.pending, (state) => {
@@ -32,12 +37,22 @@ const supplierSlice = createSlice({
         state.isLoading = false;
         state.name = action.payload.name;
         state.contactNumbersList = action.payload.contactNumbersList;
+        state.error = "";
+        Swal.fire({
+          icon: "success",
+          title: "Supplier saved successfully",
+          text: "success",
+          confirmButtonColor: "#3a3a3a",
+        });
       })
       .addCase(saveSupplier.rejected, (state, action) => {
         state.isLoading = false;
+        state.name = "";
+        state.contactNumbersList = [];
         state.error = action.error.message;
       });
   },
 });
 
+export const { resetError } = supplierSlice.actions;
 export default supplierSlice.reducer;
