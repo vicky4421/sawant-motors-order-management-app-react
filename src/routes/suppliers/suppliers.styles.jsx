e82bf0ebe68e3f980f472cc814c63b073e568517
Vivek Sawant <vicky210490@gmail.com>
2024-06-21 22:12:57 +0530

    add styles to receive suppliers

--- src/routes/suppliers/suppliers.styles.jsx
+++ src/routes/suppliers/suppliers.styles.jsx
@@ -3,31 +3,50 @@ import styled from "styled-components";
 export const SupplierContainer = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
   width: 100%;
   margin-bottom: 2rem;
 `;
 
 export const SupplierListContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
+  width: 70%;
+  padding: 0 1rem;
+`;
+
+export const SupplierDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  width: 90%;
+  padding: 0 1rem;
+  margin-bottom: 1rem;
+  border: 1px solid #3a3a3a;
+  border-radius: 0.5rem;
+`;
+
+export const LogoContainerDiv = styled.div`
+  display: flex;
+  align-items: end;
+  justify-content: end;
   width: 50%;
-  padding: 0 5rem;
+  margin: auto auto;
 `;
 
 export const SupplierName = styled.h3`
   font-size: 2rem;
   font-weight: bold;
-  /* margin-bottom: 1rem; */
+  margin-left: 0;
+  width: 50%;
 `;
 
 export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 50%;
-  padding: 0 5rem;
+  width: 30%;
+  padding: 0 1rem;
 `;
 
 export const AddSupplierForm = styled.form`
