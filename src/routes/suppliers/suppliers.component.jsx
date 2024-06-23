8ceb1d99a4f4eb3d8a5fbcca52cadd89edc5a5ec
Vivek Sawant <vicky210490@gmail.com>
2024-06-23 22:21:23 +0530

    add delete supplier, refactor expanded logic from name to id.

--- src/routes/suppliers/suppliers.component.jsx
+++ src/routes/suppliers/suppliers.component.jsx
@@ -5,6 +5,7 @@ import Swal from "sweetalert2";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 import { PropagateLoader } from "react-spinners";
+import { HashLoader } from "react-spinners";
 
 // imports from project
 import {
@@ -22,11 +23,16 @@ import {
   SearchBarContainer,
 } from "../../routes/suppliers/suppliers.styles";
 import FormInput from "../../components/form-input/form-input.component";
+import nothingHere from "../../assets/nothing-here.gif";
+import trashpng from "../../assets/trash.png";
+import arrow from "../../assets/arrow.png";
+import edit from "../../assets/edit.png";
+
+// imports from this project state
 import { saveSupplier } from "../../store/supplier/supplier.slice";
 import { getSupplier } from "../../store/supplier/supplier.slice";
 import { resetError } from "../../store/supplier/supplier.slice";
-import arrowDown from "../../assets/arrow-down.png";
-import arrowUp from "../../assets/arrow-up.png";
+import { deleteSupplier } from "../../store/supplier/supplier.slice";
 
 const Suppliers = () => {
   // local state
@@ -34,7 +40,6 @@ const Suppliers = () => {
   const [whatsappNumber, setWhatsappNumber] = useState("");
   const [alternateNumber, setAlternateNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-  const [expanded, setExpanded] = useState({ [supplierName]: false });
 
   // redux state
   const error = useSelector((state) => state.supplier.error);
@@ -42,11 +47,21 @@ const Suppliers = () => {
   const suppliers = useSelector((state) => state.supplier.suppliers);
   const dispatch = useDispatch();
 
+  // const [expanded, setExpanded] = useState({ [supplierName]: false });
+  const [expanded, setExpanded] = useState({});
+
   // toggle expanded state
-  const toggleExpanded = (supplierName) => {
+  // const toggleExpanded = (supplierName) => {
+  //   setExpanded((prevExpanded) => ({
+  //     ...prevExpanded,
+  //     [supplierName]: !prevExpanded[supplierName],
+  //   }));
+  // };
+
+  const toggleExpanded = (supplierId) => {
     setExpanded((prevExpanded) => ({
       ...prevExpanded,
-      [supplierName]: !prevExpanded[supplierName],
+      [supplierId]: !prevExpanded[supplierId],
     }));
   };
 
@@ -98,6 +113,8 @@ const Suppliers = () => {
         title: "Oops...",
         text: "Please enter whatsapp number",
         confirmButtonColor: "#3a3a3a",
+        timer: 5000,
+        timerProgressBar: true,
       });
       return;
     } else if (whatsappNumber.length !== 10) {
@@ -106,6 +123,8 @@ const Suppliers = () => {
         title: "Oops...",
         text: "Please enter valid whatsapp number",
         confirmButtonColor: "#3a3a3a",
+        timer: 5000,
+        timerProgressBar: true,
       });
       return;
     } else if (whatsappNumber === alternateNumber) {
@@ -114,6 +133,8 @@ const Suppliers = () => {
         title: "Oops...",
         text: "Please enter different alternate whatsapp number",
         confirmButtonColor: "#3a3a3a",
+        timer: 5000,
+        timerProgressBar: true,
       });
       return;
     }
@@ -132,14 +153,55 @@ const Suppliers = () => {
     resetFormFields();
   };
 
+  // handle delete
+  const handleDelete = (supplierId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+      timer: 5000,
+      timerProgressBar: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteSupplier(supplierId));
+        Swal.fire({
+          icon: "success",
+          title: "Supplier deleted successfully",
+          text: "success",
+          confirmButtonColor: "#3a3a3a",
+          timer: 5000,
+          timerProgressBar: true,
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Supplier not deleted",
+          text: "success",
+          confirmButtonColor: "#3a3a3a",
+          timer: 5000,
+          timerProgressBar: true,
+        });
+      }
+    });
+  };
+
   useEffect(() => {
     if (error) {
-      if (error === "Request failed with status code 409") {
+      if (
+        error === "Request failed with status code 409" ||
+        error === "Request failed with status code 400"
+      ) {
         Swal.fire({
           icon: "error",
           title: "Oops...",
           text: "Contact already exists!",
           confirmButtonColor: "#3a3a3a",
+          timer: 3000,
+          timerProgressBar: true,
         });
       } else {
         Swal.fire({
@@ -147,6 +209,8 @@ const Suppliers = () => {
           title: "Oops...",
           text: error,
           confirmButtonColor: "#3a3a3a",
+          timer: 3000,
+          timerProgressBar: true,
         });
       }
       dispatch(resetError());
@@ -172,47 +236,82 @@ const Suppliers = () => {
               required
             />
           </SearchBarContainer>
-          {suppliers.length === 0 && <p>No suppliers found</p>}
-          {filteredSuppliers.map((supplier) => (
-            <SupplierDiv key={supplier._id}>
-              <SupplierNameLogoContainer>
-                <SupplierName>{supplier.name}</SupplierName>
-                <LogoContainerDiv>
-                  {expanded[supplier.name] ? (
-                    <img
-                      src={arrowUp}
-                      alt="arrow-up"
-                      onClick={() => toggleExpanded(supplier.name)}
-                      height={20}
-                    />
-                  ) : (
-                    <img
-                      src={arrowDown}
-                      alt="arrow-down"
-                      onClick={() => toggleExpanded(supplier.name)}
-                      height={20}
-                    />
-                  )}
-                </LogoContainerDiv>
-              </SupplierNameLogoContainer>
-              {expanded[supplier.name] && (
-                <ContactHolderDiv>
-                  <ContactDiv>
-                    <p style={{ marginRight: "5rem" }}>Whatsapp Number</p>
-                    <p style={{ color: "blue" }}>{supplier.whatsappNumber}</p>
-                  </ContactDiv>
-                  {supplier.alternateNumber && (
-                    <ContactDiv>
-                      <p style={{ marginRight: "5rem" }}>Alternate Number</p>
-                      <p style={{ color: "blue" }}>
-                        {supplier.alternateNumber}
-                      </p>
-                    </ContactDiv>
-                  )}
-                </ContactHolderDiv>
+          {isLoading ? (
+            <HashLoader color="#36d7b7" loading={isLoading} size={150} />
+          ) : (
+            <>
+              {suppliers.length === 0 && (
+                <img src={nothingHere} alt="nothing here" />
               )}
-            </SupplierDiv>
-          ))}
+              {filteredSuppliers.map((supplier) => (
+                <SupplierDiv key={supplier.id}>
+                  <SupplierNameLogoContainer>
+                    <SupplierName>{supplier.name}</SupplierName>
+                    <LogoContainerDiv>
+                      <img
+                        src={edit}
+                        alt="edit"
+                        height={20}
+                        style={{ padding: "1rem", cursor: "pointer" }}
+                      />
+                      <img
+                        src={trashpng}
+                        alt="delete"
+                        height={20}
+                        style={{ padding: "1rem", cursor: "pointer" }}
+                        onClick={() => handleDelete(supplier.id)}
+                      />
+                      {expanded[supplier.id] ? (
+                        <img
+                          src={arrow}
+                          alt="arrow-up"
+                          onClick={() => toggleExpanded(supplier.id)}
+                          height={20}
+                          style={{
+                            padding: "1rem",
+                            cursor: "pointer",
+                            rotate: "-90deg",
+                          }}
+                        />
+                      ) : (
+                        <img
+                          src={arrow}
+                          alt="arrow-down"
+                          onClick={() => toggleExpanded(supplier.id)}
+                          height={20}
+                          style={{
+                            padding: "1rem",
+                            cursor: "pointer",
+                            rotate: "90deg",
+                          }}
+                        />
+                      )}
+                    </LogoContainerDiv>
+                  </SupplierNameLogoContainer>
+                  {expanded[supplier.id] && (
+                    <ContactHolderDiv>
+                      <ContactDiv>
+                        <p style={{ marginRight: "5rem" }}>Whatsapp Number</p>
+                        <p style={{ color: "blue" }}>
+                          {supplier.whatsappNumber}
+                        </p>
+                      </ContactDiv>
+                      {supplier.alternateNumber && (
+                        <ContactDiv>
+                          <p style={{ marginRight: "5rem" }}>
+                            Alternate Number
+                          </p>
+                          <p style={{ color: "blue" }}>
+                            {supplier.alternateNumber}
+                          </p>
+                        </ContactDiv>
+                      )}
+                    </ContactHolderDiv>
+                  )}
+                </SupplierDiv>
+              ))}
+            </>
+          )}
         </SupplierListContainer>
         <FormContainer>
           <AddSupplierForm onSubmit={handlesubmit}>
