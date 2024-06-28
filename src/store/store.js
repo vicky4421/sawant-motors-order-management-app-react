08c8ee5b71f15f93455641d1dcffa81f96319b91
Vivek Sawant <vicky210490@gmail.com>
2024-06-28 22:03:56 +0530

    import category reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,12 @@ import logger from "redux-logger";
 
 // imports from project
 import supplierReducer from "./supplier/supplier.slice";
+import categoryReducer from "./category/categorySlice";
 
 const store = configureStore({
   reducer: {
     supplier: supplierReducer,
+    category: categoryReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
