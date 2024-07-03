9694e72611ed0387e06b5f70eab5aa769d5d69a5
Vivek Sawant <vicky210490@gmail.com>
2024-07-03 22:05:46 +0530

    add product reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,14 @@ import logger from "redux-logger";
 
 // imports from project
 import supplierReducer from "./supplier/supplier.slice";
-import categoryReducer from "./category/categorySlice";
+import categoryReducer from "./category/category.slice";
+import productReducer from "./product/product.slice";
 
 const store = configureStore({
   reducer: {
     supplier: supplierReducer,
     category: categoryReducer,
+    product: productReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
