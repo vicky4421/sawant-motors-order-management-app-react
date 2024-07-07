72c9eb56e058b2b473c91879cbaf2a3479c32c42
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 22:00:03 +0530

    define new container for unit

---
+++ src/routes/products/products.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const UnitContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 2rem;
+`;
