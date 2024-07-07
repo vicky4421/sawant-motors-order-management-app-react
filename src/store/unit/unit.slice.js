754bdf00559badbf44973d86023b055e9bf29708
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 22:01:25 +0530

    define save and update unit action

--- src/store/unit/unit.slice.js
+++ src/store/unit/unit.slice.js
@@ -1,6 +1,7 @@
 // imports from third party libraries
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 // default state
 const initialState = {
@@ -15,6 +16,30 @@ export const getUnits = createAsyncThunk("unit/getUnit", async () => {
     .get("http://localhost:9000/unit/allUnits")
     .then((response) => response.data);
 });
+
+// async save unit actions
+export const saveUnit = createAsyncThunk(
+  "unit/saveUnit",
+  async (unit, { dispatch }) => {
+    const response = await axios
+      .post("http://localhost:9000/unit", unit)
+      .then((response) => response.data);
+    dispatch(getUnits());
+    return response;
+  }
+);
+
+// async update unit actions
+export const updateUnit = createAsyncThunk(
+  "unit/updateUnit",
+  async (unit, { dispatch }) => {
+    const response = await axios
+      .put(`http://localhost:9000/unit`, unit)
+      .then((response) => response.data);
+    dispatch(getUnits());
+    return response;
+  }
+);
 
 // slice
 const unitSlice = createSlice({
@@ -38,6 +63,50 @@ const unitSlice = createSlice({
         state.isLoading = false;
         state.error = action.error.message;
         state.units = [];
+      })
+      .addCase(saveUnit.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(saveUnit.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.units = state.units.concat(action.payload);
+        state.error = "";
+        Swal.fire({
+          icon: "success",
+          title: "Unit added successfully",
+          timer: 2000,
+          timerProgressBar: true,
+          confirmButtonColor: "#3a3a3a",
+        });
+      })
+      .addCase(saveUnit.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      })
+
+      .addCase(updateUnit.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateUnit.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.units = state.units.map((unit) => {
+          if (unit.id === action.payload.id) {
+            return action.payload;
+          }
+          return unit;
+        });
+        state.error = "";
+        Swal.fire({
+          icon: "success",
+          title: "Unit updated successfully",
+          timer: 2000,
+          timerProgressBar: true,
+          confirmButtonColor: "#3a3a3a",
+        });
+      })
+      .addCase(updateUnit.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
       });
   },
 });
